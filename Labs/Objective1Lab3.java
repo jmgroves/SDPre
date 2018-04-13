cbe68cf2610ea6d69bc4aaec4ca3669b7cf51281
jmgroves <jared.m.groves@gmail.com>
2018-04-13 13:46:28 -0700

    complete Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3{
+  public static void main (String[] args){
+
+    System.out.println("Name: Jared Groves");
+    System.out.println("Age: 30");
+    System.out.println("State: California");
+  }
+}
