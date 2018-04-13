afb105c11420c786ac20b4f553632ec7b5145990
jmgroves <jared.m.groves@gmail.com>
2018-04-13 13:52:32 -0700

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2{
+  public static void main(String[] args){
+
+  String name = "Jared Groves";
+  int age = 30;
+  String state = "California";
+
+  System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
