a640cbd379a43c9f7bfba599a015ed4f39b4cb21
jmgroves <jared.m.groves@gmail.com>
2018-04-14 11:53:38 -0700

    complete Objective6Lab5

---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Objective6Lab5{
+  public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    int selection = 0;
+
+    while(selection != 3){
+      System.out.println("____Menu____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+      selection = scanner.nextInt();
+
+      if(selection == 1){
+        System.out.println("Hello world!");
+      }
+      else if(selection == 2){
+        System.out.println("Pizza, Beer, Hamburgers");
+      }
+      else if(selection == 3){
+        System.out.println("Goodbye!");
+      }
+      else{
+        System.out.println("Invalid option, please try again!");
+      }
+    }
+  }
+}
