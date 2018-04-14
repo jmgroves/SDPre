c6f242bdff4a88266660736ebd723ce2e16d81f7
jmgroves <jared.m.groves@gmail.com>
2018-04-14 11:28:36 -0700

    ljthw ex 33

---
+++ JavaTheHardWay/ThereAndBackAgain.java
@@ -0,0 +1,13 @@
+public class ThereAndBackAgain{
+  public static void main(String[] args){
+    System.out.println("Here.");
+    erebor();
+    System.out.println("Back the first time.");
+    erebor();
+    System.out.println("Back the second time");
+  }
+
+  public static void erebor(){
+    System.out.println("There.");
+  }
+}
