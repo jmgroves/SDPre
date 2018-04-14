13f1877b33d74d8f66b9e8fb41a6da8e2a65e66b
jmgroves <jared.m.groves@gmail.com>
2018-04-14 11:42:18 -0700

    complete Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4{
+  public static void main(String[] args){
+    int count = 0;
+    int sum = 0;
+
+    while(count <= 20){
+      sum += count;
+      count++;
+    }
+    System.out.println(sum);
+  }
+}
